f363dc17a72ff4d19ebc6bc4509ee3f558cb6c0b
Alexandre <developpeur@alexandre-velia.com>
2024-03-18 11:21:14 +0100

    ✨ 🚧 Add PersonalityCollection, PersonalityResource, PersonalityFactory, PersonalityUpdateRequest, PersonalityController, PersonalityStoreRequest, and Personality model
    - Add PersonalityCollection to transform resource collection into an array
    - Add PersonalityResource to transform resource into an array
    - Add PersonalityFactory to define the model's default state
    - Add PersonalityUpdateRequest to validate update request
    - Add PersonalityController with index, store, show, update, and destroy methods
    - Add PersonalityStoreRequest to validate store request
    - Add Personality model with mass assignable attributes and casts

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::apiResource('personalities', App\Http\Controllers\Api\PersonalityController::class);
+
+Route::apiResource('users', App\Http\Controllers\Api\UserController::class);
