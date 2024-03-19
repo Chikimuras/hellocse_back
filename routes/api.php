b495710af37a958c90539a2a82c82ba779ba952b
Alexandre <developpeur@alexandre-velia.com>
2024-03-19 13:02:34 +0100

    🔨 Remove duplicate API resource routes for personalities and users in api.php

--- routes/api.php
+++ routes/api.php
@@ -17,13 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-
-Route::apiResource('personalities', App\Http\Controllers\Api\PersonalityController::class);
-
-Route::apiResource('users', App\Http\Controllers\Api\UserController::class);
-
-
-Route::apiResource('personalities', App\Http\Controllers\Api\PersonalityController::class);
-
-Route::apiResource('users', App\Http\Controllers\Api\UserController::class);
