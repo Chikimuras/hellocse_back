3b76164c1590e1bd41d486ba551c1eed796677f2
Alexandre <developpeur@alexandre-velia.com>
2024-03-19 13:02:26 +0100

    🔄 Rename migrations for personalities and users tables
    ℹ️ Rename migration files for personalities and users tables. Remove 'created_by'
    column from personalities table.

--- database/migrations/2024_03_18_145836_create_personalities_table.php
+++ database/migrations/2024_03_18_145836_create_personalities_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->date('deathdate')->nullable();
             $table->text('description');
             $table->string('image')->nullable();
-            $table->unsignedInteger('created_by');
             $table->foreignId('user_id');
             $table->timestamps();
         });
