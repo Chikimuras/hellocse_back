0da4e0f42b3391d5a4c5c1104fb58c771ed3b4b9
Alexandre <developpeur@alexandre-velia.com>
2024-03-19 13:04:44 +0100

    🔄 Update dependencies in composer.lock and composer.json. Add Intervention Image, Intervention Image Laravel, and related packages. Add image.php configuration file. Add 'personalities' link in filesystems.php. Adjust routes in web.php to include PersonalityController.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PersonalityController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -16,23 +17,34 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::get('/', [PersonalityController::class, 'index'])->name('personality.index');
 
 Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
+    return Inertia::render('Dashboard',
+        [
+            'sheetCount' => \App\Models\Personality::all()->count(),
+            'distinctContributors' => \App\Models\Personality::all()->unique('user_id')->count(),
+            'sheetWrittenCount' => \App\Models\Personality::all()->where('user_id', auth()->id())->count(),
+        ]);
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    //Grouped routes with prefix 'personality' and name 'personality.'.
+    Route::prefix('personality')->name('personality.')->group(function () {
+        Route::get('/create', [PersonalityController::class, 'create'])->name('create');
+        Route::post('/', [PersonalityController::class, 'store'])->name('store');
+        Route::get('/{personality}/edit', [PersonalityController::class, 'edit'])->name('edit');
+        Route::patch('/{personality}', [PersonalityController::class, 'update'])->name('update');
+        Route::delete('/{personality}', [PersonalityController::class, 'destroy'])->name('destroy');
+    });
 });
 
 require __DIR__.'/auth.php';
+
+
+
+
+
